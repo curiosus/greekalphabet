b05ebf5c9960cfe88ad1e4d0e5ffb49772023e20
curiosus <curiosus@gmail.com>
2022-03-08 09:26:23 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div>
+			<h1>Greek Alphabet</h1>
+		</div>
+	);
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			}	
+		]	
+	},
+	plugins: [
+		new HtmlWebpackPlugin(
+			{
+				template: path.resolve(__dirname, "src", "index.html")	
+			}	
+		)	
+	]
+};
